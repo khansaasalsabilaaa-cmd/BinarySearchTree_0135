975039ba9054c115fef0e518f096535b4dc92e44
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-06-11 11:34:20 +0700

    menambahkan case 4

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -166,3 +166,9 @@ int main()
                 obj.preorder(obj.ROOT);
                 break;
             }
+
+            case '4':
+            {
+                obj.postorder(obj.ROOT);
+                break;
+            }
